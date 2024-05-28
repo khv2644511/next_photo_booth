2bbe308c03c28fb397ebfbca51760693ad96cb62
hyebin9808 <khv0804@yolcargo.com>
2024-05-28 15:29:23 +0900

    Modifier: hover 및 다크모드 상태 시 스타일 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,29 @@
 export default function Home() {
     return (
-        <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
-            <div className="bg-white w-full shadow-lg p-5 rounded-2xl">
+        <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+            <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
                 <div className="flex justify-between items-center">
                     <div className="flex flex-col">
-                        <span className="text-gray-600 font-semibold -mb-1">In transit</span>
-                        <span className="text-4xl font-semibold">Coolblue</span>
+                        <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">In transit</span>
+                        <span className="text-4xl font-semibold dark:text-white">Coolblue</span>
                     </div>
                     <div className="size-12 bg-orange-400 rounded-full" />
                 </div>
                 <div className="my-2 flex items-center gap-2">
-                    <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">Today</span>
-                    <span>9:30-10:30</span>
+                    <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full hover:bg-green-500 hover:scale-125 transition">
+                        Today
+                    </span>
+                    <span className="dark:text-gray-100">9:30-10:30</span>
                 </div>
                 <div className="relative">
                     <div className="absolute bg-gray-200 w-full rounded-full h-2" />
                     <div className="absolute bg-green-400 w-2/3 rounded-full h-2" />
                 </div>
-                <div className="flex justify-between items-center mt-5 text-gray-600">
+                <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
                     <span>Expected</span>
                     <span>Sorting center</span>
                     <span>In transit</span>
-                    <span className="text-gray-400">Delivered</span>
+                    <span className="text-gray-400 dark:text-gray-500">Delivered</span>
                 </div>
             </div>
         </main>
