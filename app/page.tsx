df972d574246b1c3cf6376e7ad67c2a584215d46
hyebin9808 <khv0804@yolcargo.com>
2024-05-28 16:52:00 +0900

    Tainwind를 통한 반응형 디자인

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
     return (
-        <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+        <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
             <div
                 className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600
-            flex flex-col gap-2"
+            flex flex-col gap-2 md:flex-row"
             >
                 <input
                     className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
                     type="text"
                     placeholder="Search here.."
                 />
-                <button className="bg-black bg-opacity-80 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90">
+                <button className="bg-black bg-opacity-80 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 md:px-10">
                     Search
                 </button>
             </div>
