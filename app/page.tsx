11194cc8b8dc08cb9ee494ceeb520b09cc0253a8
hyebin9808 <khv0804@yolcargo.com>
2024-05-29 13:37:58 +0900

    Tailwind: State Modifiers(has, *)

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
         <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
             <div
                 className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600
-            flex flex-col gap-2 md:flex-row"
+            flex flex-col gap-2 md:flex-row *:outline-none has-[:invalid]:ring-red-300 has-[:invalid]:ring ring ring-transparent transition-shadow"
             >
                 <input
-                    className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+                    className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
                     required
                     type="email"
                     placeholder="Email Address"
