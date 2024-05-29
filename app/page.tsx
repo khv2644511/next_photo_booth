6a3749c2a7be6207d0e50a9e7f35499277dd2cb8
hyebin9808 <khv0804@yolcargo.com>
2024-05-29 10:23:17 +0900

    Tailwind의 peer/peer-invalid:, ring, focus: 등의 Modifier를 통한 Form 스타일링

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,16 @@ export default function Home() {
             flex flex-col gap-2 md:flex-row"
             >
                 <input
-                    className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-                    type="text"
-                    placeholder="Search here.."
+                    className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+                    required
+                    type="email"
+                    placeholder="Email Address"
                 />
-                <button className="bg-black bg-opacity-80 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 md:px-10">
-                    Search
+                <span className="text-red-500 font-medium hidden peer-invalid:block">
+                    Email is required.
+                </span>
+                <button className="bg-gradient-to-tr from-cyan-500 to-purple-400 via-pink-300 bg-opacity-80 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 md:px-10 peer-invalid:from-red-500 peer-invalid:bg-orange-400">
+                    Log in
                 </button>
             </div>
         </main>
