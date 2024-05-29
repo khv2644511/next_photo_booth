59ba34241e822fbaeb223b51b515176424d5140e
hyebin9808 <khv0804@yolcargo.com>
2024-05-29 16:14:03 +0900

    Tailwind: Lists and Animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
 export default function Home() {
     return (
-        <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+        <main
+            className="bg-gray-100 sm:bg-red-100
+         md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100
+          2xl:bg-purple-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
             <div
                 className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600
-            flex flex-col gap-2 md:flex-row *:outline-none has-[:invalid]:ring-red-300 has-[:invalid]:ring ring ring-transparent transition-shadow"
-            >
-                <input
-                    className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-                    required
-                    type="email"
-                    placeholder="Email Address"
-                />
-                <span className="text-red-500 font-medium hidden peer-invalid:block">
-                    Email is required.
-                </span>
-                <button className="bg-gradient-to-tr from-cyan-500 to-purple-400 via-pink-300 bg-opacity-80 text-white py-2 rounded-full active:scale-90 transition-transform font-medium focus:scale-90 md:px-10 peer-invalid:from-red-500 peer-invalid:bg-orange-400">
-                    Log in
-                </button>
+            flex flex-col gap-3 ">
+                {['hyeppy', 'Me', 'You', 'Yourself'].map((person, index) => (
+                    <div
+                        key={index}
+                        className="flex items-center gap-5  p-2.5 rounded-xl  border-b-2 pb-5 last:border-0 last:pb-0">
+                        <div className="size-10 bg-blue-400 rounded-full" />
+                        <span className="text-lg font-medium">{person}</span>
+                        <div className="size-6  bg-red-500 text-white flex items-center justify-center rounded-full relative">
+                            <span>{index}</span>
+                            <div className="bg-red-500 size-6 rounded-full absolute animate-ping" />
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     );
