5d40c315b356d8edcd85ef92c4b64075ea893d41
hyebin9808 <khv0804@yolcargo.com>
2024-05-30 11:11:08 +0900

    depend: prettier 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
 export default function Home() {
-    return (
-        <main
-            className="bg-gray-100 sm:bg-red-100
-         md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100
-          2xl:bg-purple-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-            <div
-                className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600
-            flex flex-col gap-3 ">
-                {['hyeppy', 'Me', 'You', 'Yourself'].map((person, index) => (
-                    <div
-                        key={index}
-                        className="flex items-center gap-5  p-2.5 rounded-xl  border-b-2 pb-5 last:border-0 last:pb-0">
-                        <div className="size-10 bg-blue-400 rounded-full" />
-                        <span className="text-lg font-medium">{person}</span>
-                        <div className="size-6  bg-red-500 text-white flex items-center justify-center rounded-full relative">
-                            <span>{index}</span>
-                            <div className="bg-red-500 size-6 rounded-full absolute animate-ping" />
-                        </div>
-                    </div>
-                ))}
+  return (
+    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-gray-700">
+      <div className="flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg dark:bg-gray-600 ">
+        {['hyeppy', 'Me', 'You', 'Yourself'].map((person, index) => (
+          <div
+            key="{index}"
+            className="group flex items-center gap-5 rounded-xl border-b-2  p-2.5 pb-5 last:border-0 last:pb-0"
+          >
+            <div className="size-10 rounded-full bg-blue-400" />
+            <span className="text-lg font-medium group-hover:text-red-400">
+              {person}
+            </span>
+            <div className="relative flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
+              <span>{index}</span>
+              <div className="absolute size-6 animate-ping rounded-full bg-red-500" />
             </div>
-        </main>
-    );
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
