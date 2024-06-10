9616a15eb1d10cc543802ff3773df0233c48b883
hyebin9808 <khv0804@yolcargo.com>
2024-06-10 14:46:15 +0900

    feat: zod object로 form validation 설정하기

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,7 +1,12 @@
 'use server';
 import { z } from 'zod';
 
-const usernameSchema = z.string().min(5).max(10);
+const fomrSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+});
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -11,7 +16,20 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get('confirm_password'),
   };
 
-  console.log(data);
+  //   console.log(data);
 
-  usernameSchema.parse(data.username);
+  // parse는 에러를 throw하기 때문에 try,catch를 사용하지 않으면 에러화면이 뜨게됨
+  //   try {
+  //     fomrSchema.parse(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+
+  // safeParse는 parse와 달리 에러를 던지지 않음
+  const result = fomrSchema.safeParse(data);
+  console.log(result); // { success: false, error: [Getter] }
+  if (!result.success) {
+    // console.log(result.error.flatten());
+    return result.error.flatten();
+  }
 }
