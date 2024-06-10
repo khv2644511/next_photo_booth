8a921b11794b3b6ed94a6cc9064feed2896dec25
hyebin9808 <khv0804@yolcargo.com>
2024-06-10 15:28:51 +0900

    feat: zod refine을 통해 필드/오브젝트의 커스텀된 validation과 error message 표시하기

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,12 +1,39 @@
 'use server';
 import { z } from 'zod';
 
-const fomrSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes('potato');
+
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const fomrSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: 'Username must be a string!',
+        required_error: 'Where is my username?',
+      })
+      .min(3, 'too short!!!!!')
+      .max(10, 'That is too looooong')
+      .refine(checkUsername, 'No potatos allowed!'),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswords, {
+    message: 'Both password sould be same!',
+    path: ['confirm_password'],
+  });
+
+// 마지막 refine 함수는 form 객체 전체에 대한 유효성을 검사해,
+// 모든 데이터를 받을 수 있다(따라서 password, confirm_password 인자를 모두 받을 수 있다.)
+// 이때, form에 대한 에러메세지는 fieldError가 아닌 formError에 들어가 화면에 보이지 않는다.
+// 이를 해결하기 위해, path로 에러가 보여질 위치를 작성할 수 있다.
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -29,7 +56,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = fomrSchema.safeParse(data);
   console.log(result); // { success: false, error: [Getter] }
   if (!result.success) {
-    // console.log(result.error.flatten());
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
