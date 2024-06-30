d71ad5bcf7d89a6273c23a1ae850a99b1d54cd41
hyebin9808 <khv0804@yolcargo.com>
2024-06-30 20:30:58 +0900

    Prisma Realationships and onDelete

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,18 +1,35 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-// const db = new PrismaClient()
-
+const db = new PrismaClient();
 
 // async function test() {
-//     const user = await db.user.create({
-//         data: {
-//             username: "test",
-//         }
-//     })
+//   const token = await db.sMSToken.create({
+//     data: {
+//       token: '12342123',
+//       user: {
+//         connect: {
+//           id: 3,
+//         },
+//       },
+//     },
+//   });
 
-//     console.log(user)
+//   console.log(token);
 // }
 
-// test()
+async function test() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
 
-export default db
+  console.log(token);
+}
+
+test();
+
+export default db;
