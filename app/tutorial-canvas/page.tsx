9c02f59f2d05c2b6406fec43954f73062a05f54b
hyebin9808 <khv0804@yolcargo.com>
2024-09-25 14:48:37 +0900

    feat: canvas로 이미지에 액자 씌우기(이미지 슬라이싱 이용)

---
+++ app/tutorial-canvas/page.tsx
@@ -0,0 +1,87 @@
+// 'use client';
+
+// import { useEffect } from 'react';
+
+// export default function Canvas() {
+//   useEffect(() => {
+//     function draw() {
+//       var canvas = document.querySelector('canvas');
+//       if (canvas.getContext) {
+//         var ctx = canvas.getContext('2d');
+//         for (var i = 0; i <= 4; i++) {
+//           for (var j = 0; j <= 3; j++) {
+//             ctx.beginPath();
+//             var x = 25 + j * 50; // x coordinate
+//             var y = 25 + i * 50; // y coordinate
+//             var radius = 20; // Arc radius
+//             var startAngle = 0; // Starting point on circle
+//             var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+//             var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
+
+//             ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+//             if (i > 1) {
+//               ctx.fill();
+//             } else {
+//               ctx.stroke();
+//             }
+//           }
+//         }
+//       }
+//     }
+//     draw();
+//   }, []);
+//   return (
+//     <div className="flex flex-col gap-10 py-8 px-6">
+//       {/* <canvas></canvas> */}
+//       <canvas id="my-house" width="300" height="300"></canvas>
+//       {/* <canvas id="canvas" width="150" height="150"></canvas> */}
+//     </div>
+//   );
+// }
+'use client';
+
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+
+export default function Canvas() {
+  const canvasRef = useRef(null); // Reference for the canvas
+  const picRef = useRef(null); // Reference for the picture
+  const frameRef = useRef(null); // Reference for the frame
+
+  useEffect(() => {
+    function draw() {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+
+      // Draw frame
+      ctx.drawImage(document.getElementById('frame'), 0, 0, 132, 150);
+      // Draw slice
+      ctx.drawImage(document.getElementById('source'), 30, 30, 70, 90);
+    }
+
+    draw();
+  }, []);
+
+  return (
+    <div className="flex flex-col mx-auto">
+      <canvas id="canvas" ref={canvasRef}></canvas>
+      <Image
+        ref={picRef}
+        id="source"
+        src="https://mdn.github.io/shared-assets/images/examples/rhino.jpg"
+        width="300"
+        height="227"
+        alt="image"
+      />
+      <Image
+        ref={frameRef}
+        id="frame"
+        src="https://img.freepik.com/premium-photo/old-wooden-frame_1203-2205.jpg"
+        width={132}
+        height={150}
+        alt="frame"
+      />
+    </div>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // HTTPS 프로토콜 사용
+        hostname: 'mdn.github.io', // 외부 이미지의 호스트네임
+        port: '', // 포트 설정은 비어 둡니다 (기본값 사용)
+        pathname: '/shared-assets/images/examples/**', // 경로 설정
+      },
+      {
+        protocol: 'https', // HTTPS 프로토콜 사용
+        hostname: 'img.freepik.com',
+        port: '', // 포트 설정은 비어 둡니다 (기본값 사용)
+        pathname: '/premium-photo/**', // 경로 설정
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
