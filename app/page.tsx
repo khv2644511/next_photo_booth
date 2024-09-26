544a9b940c25292f091c0415d3eeb427383d165e
hyebin9808 <khv0804@yolcargo.com>
2024-09-26 15:43:43 +0900

    fix: 메인 페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,26 @@ import './lib/db';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6">
-      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <span className="text-9xl">🥕</span>
-        <h1 className="text-4xl">당근</h1>
-        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium *:text-black">
+        <span className="text-9xl">🥕🥕🥕</span>
+        <h1 className="text-4xl">Hi</h1>
+        <h2 className="text-2xl">Welcome to hyeppy's photo booth 😊;</h2>
+        <h2 className="text-2xl">Do you want to take photo with me? 📸;</h2>
       </div>
 
-      <div className="flex flex-col items-center gap-3 w-full">
-        <Link href="/create-account" className="primary-btn py-2.5 text-lg">
+      <div className="flex flex-col items-center gap-3 w-full *:text-black">
+        {/* <Link href="/create-account" className="primary-btn py-2.5 text-lg">
+          시작하기
+        </Link> */}
+        <Link href="/photo-booth" className="primary-btn py-2.5 text-lg">
           시작하기
         </Link>
-        <div className="flex gap-2">
+        {/* <div className="flex gap-2">
           <span>이미 계정이 있나요?</span>
           <Link href="/login" className="hover:underline">
             로그인
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
