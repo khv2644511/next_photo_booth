588f5191f01e16ced6043c0c57fa34faaf6360c7
hyebin9808 <khv0804@yolcargo.com>
2024-10-07 16:33:50 +0900

    fix: 비디오 거울모드 및 버튼 css fixed로 고정

--- app/photo-booth/page.tsx
+++ app/photo-booth/page.tsx
@@ -12,10 +12,9 @@ import React from 'react';
 import Webcam from 'react-webcam';
 
 export default function PhotoBooth() {
-  const videoRef = useRef(null) as any; // Reference to the video element
-  const [inputValue, setInputValue] = useState(0); // State to handle the range input
-  const [imageCapture, setImageCapture] = useState(null); // State to store imageCapture instance
-  const [isReady, setIsReady] = useState(false); // State to track if imageCapture is ready
+  const videoRef = useRef(null) as any;
+  const [imageCapture, setImageCapture] = useState(null);
+  const [isReady, setIsReady] = useState(false);
   const [capturedImg, setCapturedImg] = useState([]);
   const [count, setCount] = useState(1);
   const container = useRef(null);
@@ -36,8 +35,6 @@ export default function PhotoBooth() {
       scrollX: 0, // Prevents capturing any scrolled positions
       scrollY: 0, // Prevents capturing any scrolled positions
     }).then(function (canvas) {
-      //   document.body.appendChild(canvas);
-
       // Convert the canvas to a data URL
       var imageData = canvas.toDataURL('image/jpeg');
 
@@ -55,19 +52,19 @@ export default function PhotoBooth() {
     facingMode: 'user',
   };
 
-  const WebcamCapture = () => {
-    const webcamRef = useRef(null);
-    const capture = useCallback(() => {
-      const imageSrc = webcamRef.current.getScreenshot();
-      if (url.length > 2) {
-        return;
-      }
-      if (imageSrc) {
-        setUrl((prevUrls) => [...prevUrls, imageSrc]); // Append the new image to the array
-      }
-    }, [webcamRef]);
+  const webcamRef = useRef(null);
 
-    // console.log(url);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (url.length > 2) {
+      return;
+    }
+    if (imageSrc) {
+      setUrl((prevUrls) => [...prevUrls, imageSrc]); // Append the new image to the array
+    }
+  }, [webcamRef]);
+
+  const WebcamCapture = () => {
     return (
       <>
         <Webcam
@@ -77,113 +74,112 @@ export default function PhotoBooth() {
           screenshotFormat="image/jpeg"
           width={320}
           videoConstraints={videoConstraints}
+          style={{
+            transform: 'rotate(0, 180deg)',
+            '-webkit-transform': 'rotateY(180deg)', // Safari and Chrome
+            '-moz-transform': 'rotateY(180deg)', // Firefox
+          }}
         />
-        <div className="flex gap-10 justify-center my-5">
-          <button
-            className="min-w-32 bg-red-300 py-3 px-2 rounded-md"
-            onClick={capture}
-          >
-            Capture photo
-          </button>
-          <button
-            className="min-w-32 bg-red-300 py-3 px-2 rounded-md"
-            onClick={dowonload}
-          >
-            dowonload
-          </button>
-        </div>
-        <div className="m-auto">
-          <button
-            onClick={reset}
-            className="mb-3 bg-red-300 py-3 px-2 rounded-md"
-          >
-            다시찍기
-          </button>
-        </div>
       </>
     );
   };
 
   return (
-    <div className="flex flex-col self-center justify-center">
-      {/* <div className="w-full h-20 bg-black fixed bottom-0"></div> */}
-      <div className="w-80 flex flex-col justify-center self-center ml-10 mt-20">
-        <WebcamCapture />
-      </div>
+    <>
+      <div className="flex flex-col self-center justify-center">
+        {/* <div className="w-full h-20 bg-black fixed bottom-0"></div> */}
+        <div className="w-80 flex flex-col justify-center self-center ml-10 mt-20">
+          <WebcamCapture />
+        </div>
 
-      <div
-        ref={container}
-        className="container top-1/2 left-0 
-        w-auto h-full bg-black mx-auto p-8 flex flex-col gap-10 overflow-hidden z-20"
-      >
         <div
-          className="bg-red-100 canvas-container relative after:content-[''] after:bg-flower-frame  after:absolute after:h-full after:w-full after:bg-contain after:top-0 after:rotate-180 after:z-0"
-          style={{
-            height: '320px',
-            width: '320px',
-          }}
+          ref={container}
+          className="container top-1/2 left-0 
+          w-auto h-full bg-black mx-auto p-8 flex flex-col gap-10 overflow-hidden z-20"
         >
-          <img
-            src={url[0]}
-            alt="Screenshot"
+          <div
+            className="bg-red-100 canvas-container relative after:content-[''] after:bg-flower-frame  after:absolute after:h-full after:w-full after:bg-contain after:top-0 after:rotate-180 after:z-0"
             style={{
               height: '320px',
               width: '320px',
             }}
-          />
-
-          {/* <canvas id="canvas1" width={320} height={228}></canvas> */}
-        </div>
-        <div
-          className="bg-red-100 canvas-container relative after:content-[''] after:bg-profile-frame  after:absolute after:h-2/3 after:w-2/3 after:bg-contain after:bg-no-repeat after:bottom-0 after:z-0 before:content-['나랑사진찍을래?ㅎ'] before:w-full before:text-black before:text-xl before:absolute before:left-4 before:top-4 before:z-50"
-          style={{ height: '320px', width: '320px' }}
-        >
-          {url[1] && (
+          >
             <img
-              src={url[1]}
-              alt="Screenshot"
-              style={{
-                height: '320px',
-                width: '320px',
-              }}
-              // style={{
-              //   backgroundImage: `url(${url[1]})`,
-              //   height: '320px',
-              //   width: '320px',
-              //   backgroundSize: 'cover',
-              // }}
-            />
-          )}
-
-          {/* <canvas id="canvas2" width={320} height={228}></canvas> */}
-        </div>
-        <div
-          className="bg-red-100 canvas-container relative"
-          style={{ height: '320px', width: '320px' }}
-        >
-          <Image
-            src={ShipImage}
-            width={150}
-            alt="ship"
-            className="absolute -bottom-20 -right-6"
-          />
-          {url[2] && (
-            <img
-              src={url[2]}
+              src={url[0]}
               alt="Screenshot"
               style={{
                 height: '320px',
                 width: '320px',
               }}
             />
-          )}
 
-          {/* <canvas id="canvas3" width={320} height={228}></canvas> */}
+            {/* <canvas id="canvas1" width={320} height={228}></canvas> */}
+          </div>
+          <div
+            className="bg-red-100 canvas-container relative after:content-[''] after:bg-profile-frame  after:absolute after:h-2/3 after:w-2/3 after:bg-contain after:bg-no-repeat after:bottom-0 after:z-0 before:content-['나랑사진찍을래?ㅎ'] before:w-full before:text-black before:text-xl before:absolute before:left-4 before:top-4 before:z-50"
+            style={{ height: '320px', width: '320px' }}
+          >
+            {url[1] && (
+              <img
+                src={url[1]}
+                alt="Screenshot"
+                style={{
+                  height: '320px',
+                  width: '320px',
+                }}
+              />
+            )}
+          </div>
+          <div
+            className="bg-red-100 canvas-container relative"
+            style={{ height: '320px', width: '320px' }}
+          >
+            <Image
+              src={ShipImage}
+              width={150}
+              alt="ship"
+              className="absolute -bottom-20 -right-6"
+            />
+            {url[2] && (
+              <img
+                src={url[2]}
+                alt="Screenshot"
+                style={{
+                  height: '320px',
+                  width: '320px',
+                }}
+              />
+            )}
+
+            {/* <canvas id="canvas3" width={320} height={228}></canvas> */}
+          </div>
+          <div className="flex justify-center py-9 text-3xl">
+            혜빈이의 인생세컷
+          </div>
         </div>
-        <div className="flex justify-center py-9 text-3xl">
-          혜빈이의 인생세컷
+        <div className="fixed z-50 bg-white bottom-0 py-5 w-full">
+          <div className="flex gap-10 justify-center self-center">
+            <button
+              className="min-w-32 bg-red-300 py-3 px-2 rounded-md"
+              onClick={capture}
+            >
+              Capture photo
+            </button>
+            <button
+              className="min-w-32 bg-red-300 py-3 px-2 rounded-md"
+              onClick={dowonload}
+            >
+              dowonload
+            </button>
+            <button
+              onClick={reset}
+              className="min-w-32 bg-red-300 py-3 px-2 rounded-md"
+            >
+              다시찍기
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
