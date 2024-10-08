8e1ce5d24de1b58ed8d86d6cbfa9def2fbf6b60e
hyebin9808 <khv0804@yolcargo.com>
2024-10-08 13:52:16 +0900

    build 수정

--- components/drag.tsx
+++ components/drag.tsx
@@ -3,7 +3,7 @@
 import { useSpring, animated } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 
-export default function Drag(props) {
+export default function Drag(props: any) {
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
 
   const bind = useDrag(({ event, offset: [x, y] }) => {
